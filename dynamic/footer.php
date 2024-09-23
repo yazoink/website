02b2cd9584e9c3224105bd11cc78da28b0354ecd
yazoink <yazoink@firemail.cc>
2024-09-23 16:53:26 +1000

    a

--- dynamic/footer.php
+++ dynamic/footer.php
@@ -1,5 +1,5 @@
 	<div class="footer">
-<p>yazoink 2022-2024 &#128924; <a href=<?php echo "'$email'"; ?>><?php echo "$email"; ?></a></p>
+<p>yazoink 2022-2024 &#128924; <a href='mailto:<?php echo "$email"; ?>'><?php echo "$email"; ?></a></p>
 	</div>
     </div>
   </body>
