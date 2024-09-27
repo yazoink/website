f94c816b8436cad9903996836420a8e66110c1e1
yazoink <yazoink@firemail.cc>
2024-09-27 22:17:49 +1000

    a

--- dynamic/services.php
+++ dynamic/services.php
@@ -9,6 +9,8 @@ $(document).ready(function(){
 <h1>Services...</h1>
 <p>I host a few instances of open source frontends for various popular websites. You can use addons like <a href='https://libredirect.github.io/' target='_blank'>Libredirect</a> to automatically redirect links from the original sites to the alternatives.</p>
 <br />
+<p>(the status indicators sometimes gives false negatives, idk how to fix it)</p>
+<br />
 <div id='refresh'>
 <?php
 printServices($services);
