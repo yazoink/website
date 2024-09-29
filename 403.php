bd114b377d9620c0f53450d54e21cee866a4ce41
yazoink <yazoink@firemail.cc>
2024-09-29 14:49:26 +1000

    a

---
+++ 403.php
@@ -0,0 +1,50 @@
+<?php
+require "dynamic/variables.php";
+require "dynamic/functions.php";
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>&#128924; yazoink &#128924;</title>
+    <style>
+      @import url("style/colors.css");
+      @import url("style/fonts.css");
+
+      html {
+          background-color: var(--bg);
+          text-align: center;
+      }
+
+      body {
+          font-family: redaction;
+          font-size: 1em;
+          color: var(--fg);
+          word-wrap: break-word;
+          margin: auto;
+          min-width: 510px;
+          max-width: 510px;
+          max-height: 510px;
+          min-height: 510px;
+      }
+
+      a {
+        color: var(--fg);
+      }
+
+      h1, h2, h3, h4, ul, p {
+          margin-top: 0;
+          padding-top: 0;
+          margin-bottom: 0;
+          padding-bottom: 0;
+      }
+    </style>
+  </head>
+
+<h1>403 lolnicetry</h1>
+<h2>Go back <a href='index.php'>home</a>.</h2>
+
+<?php
+printRandomImage();
+echo "<br />";
+require "dynamic/footer.php";
+?>
