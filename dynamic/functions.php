1b18cba6e392291a2111d23be6fd360bc7a166bb
yazoink <yazoink@firemail.cc>
2024-09-29 15:44:59 +1000

    a

--- dynamic/functions.php
+++ dynamic/functions.php
@@ -3,7 +3,7 @@ function randomFromArrayButton($array, $buttonText, $buttonId) {
   $valuesArray = array_values($array);
   $jsonArray = json_encode($valuesArray);
   echo "<p><button id='$buttonId' onclick='openInNewTab_$buttonId()'>$buttonText</button></p>";
-  echo "<script>
+  echo "<script nonce='jquery'>
     document.addEventListener('DOMContentLoaded', function() {
       var urls_$buttonId = $jsonArray;
       window['openInNewTab_$buttonId'] = function() {
