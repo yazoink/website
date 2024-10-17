2f7b58224911af2a0b5d4b7d01dfa416ac544dea
yazoink <yazoink@firemail.cc>
2024-10-18 00:31:56 +1100

    a

--- js/copy-url.js
+++ js/copy-url.js
@@ -1,9 +1,9 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
 const copyURL = async (url) => {
   navigator.clipboard.writeText(url);
-  document.getElementById("copy-url").innerHTML = "&#x2398; Copied!"; 
+  document.getElementById("copy-url").innerHTML = "<b>&#x2398; Copied!</b>"; 
   await delay(2000);
-  document.getElementById("copy-url").innerHTML = "&#x2398; Copy URL"; 
+  document.getElementById("copy-url").innerHTML = "<b>&#x2398; Copy URL</b>"; 
 }
 
 document.getElementById("copy-url").addEventListener("click", copyURL);
