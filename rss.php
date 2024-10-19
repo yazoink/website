7dd3c864e1be049af226f68f98241e25ad2adff2
yazoink <yazoink@firemail.cc>
2024-10-19 17:52:35 +1100

    a

--- rss.php
+++ rss.php
@@ -1,9 +1,6 @@
 <?php
 require 'dynamic/variables.php';
 
-$jsonStr = file_get_contents('json/blog.json');
-$json = json_decode($jsonStr, true);
-
 header("Content-Type: application/rss+xml; charset=UTF-8");
 echo "<?xml version='1.0' encoding='UTF-8'?>";
 echo "<rss version='2.0'>";
@@ -12,9 +9,10 @@ echo "<title>yazoink</title>";
 echo "<link>https://$domain</link>";
 echo "<description>yazoink's blog</description>";
 foreach ($json as $blogEntry) {
+  $entry = str_replace(" ", "-", strtolower($blogEntry['title']));
   echo "<item>";
   echo "<title>" . $blogEntry['title'] . "</title>";
-  echo "<link>https://$domain/index.php?nav=Blog&amp;entry=" . $blogEntry['entry'] . "</link>";
+  echo "<link>https://$domain/index.php?nav=Blog&amp;entry=" . $entry . "</link>";
   echo "<description>" . $blogEntry['subheading'] . "</description>";
   echo "<pubDate>" . date(DATE_RSS, strtotime($blogEntry['date'])) . "</pubDate>";
   echo "</item>";
