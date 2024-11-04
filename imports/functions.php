1f21ff344f4619b3638925ab16adc4095ed70023
yazoink <yazoink@firemail.cc>
2024-11-04 22:18:15 +1100

    tweaks

--- imports/functions.php
+++ imports/functions.php
@@ -69,7 +69,7 @@ function printRandomImage() {
   $imageDir = "images/random-images";
   $images = scandir($imageDir);
   $images = array_slice($images, 2);
-  echo "<p class='center'><img src='" . $imageDir . "/" . $images[array_rand($images)] . "'></p>";
+  echo "<p><img class='center' src='" . $imageDir . "/" . $images[array_rand($images)] . "'></p>";
 }
 
 function printUrlList($urlList, $openInNewTab) {
